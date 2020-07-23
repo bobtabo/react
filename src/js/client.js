a8c7a33ea4ad64e9743186b82d438e0295ee4ceb
Satoshi Nagashiba <bobtabo.buhibuhi@gmail.com>
2020-07-24 04:28:00 +0900

    インデント修正

--- src/js/client.js
+++ src/js/client.js
@@ -1,46 +1,45 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 export default class UserList extends Component {
+  constructor() {
+      super();
 
-    constructor() {
-        super();
+      this.state = {
+        datas: []
+      };
+  }
 
-        this.state = {
-          datas: []
-        };
-    }
-
-    componentDidMount() {
-      axios
-          .get('http://laravel5.local/api/users')
-          .then(response => {
-              this.setState({datas: response.data});
-          })
-          .catch(() => {
-              console.log('通信に失敗しました');
-          });
-    }
-
-    renderDatas() {
-      return this.state.datas.map(data => {
-          return (
-              <li key={data.id}>
-                {data.id} / {data.name} / {data.email} / {data.password}
-              </li>
-          );
+  componentDidMount() {
+    axios
+      .get('http://laravel5.local/api/users')
+      .then(response => {
+          this.setState({datas: response.data});
+      })
+      .catch(() => {
+          console.log('通信に失敗しました');
       });
-    }
+  }
 
-    render() {
-      return (
-          <div className="container">
-              <ul>
-                  {this.renderDatas()}
-              </ul>
-          </div>
-      );
+  renderDatas() {
+    return this.state.datas.map(data => {
+        return (
+            <li key={data.id}>
+              {data.id} / {data.name} / {data.email} / {data.password}
+            </li>
+        );
+    });
+  }
+
+  render() {
+    return (
+        <div className="container">
+            <ul>
+                {this.renderDatas()}
+            </ul>
+        </div>
+    );
   }
 }
 
