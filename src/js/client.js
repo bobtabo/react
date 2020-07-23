0774cea108c89c6aa7f3e85ed5867a468dcbc2d1
Satoshi Nagashiba <bobtabo.buhibuhi@gmail.com>
2020-07-24 04:05:34 +0900

    API接続

--- src/js/client.js
+++ src/js/client.js
@@ -1,13 +1,58 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { Component, Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-class Layout extends React.Component {
-  render() {
-    return (
-      <h1>Welcome!</h1>
-    );
+export default class UserList extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+          datas: []
+        };
+    }
+
+    componentDidMount() {
+      axios
+          // .get('http://laravel5.local/api/users')
+          // .then(response => {
+          //     this.setState({datas: response.data});
+          // })
+          .get('http://laravel5.local/api/users')
+          .then(response => {
+            const data = response.data;
+            console.log(data);
+            this.setState({
+              datas: [...data]
+            });
+          })
+          .catch(() => {
+              console.log('通信に失敗しました');
+          });
+    }
+
+    renderDatas() {
+      return this.state.datas.map(data => {
+          return (
+              // <li>
+              //   {data.id} / {data.name} / {data.emai} / {data.password}
+              // </li>
+              <li key={data.id}>{data.name}</li>
+          );
+      });
+    }
+
+    render() {
+      return (
+          <div className="container">
+              <ul>
+                  {this.renderDatas()}
+              </ul>
+          </div>
+      );
   }
 }
 
-const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+if (document.getElementById('content')) {
+  ReactDOM.render(<UserList />, document.getElementById('content'));
+}
